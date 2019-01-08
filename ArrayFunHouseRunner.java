ce012c0654a63c3e9532e56369aa938523a84822
MrsHines <34042849+MrsHines@users.noreply.github.com>
2019-01-08 10:08:19 -0600

    Starter File

--- ArrayFunHouseRunner.java
+++ ArrayFunHouseRunner.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -11,12 +11,14 @@ public class ArrayFunHouseRunner
 	public static void main( String args[] )
 	{
 		int[] one = {4,10,0,1,7,6,5,3,2,9};
+		
+		ArrayFunHouse test = new ArrayFunHouse(one);
 
 		System.out.println(Arrays.toString(one));
-		System.out.println("sum of spots 3-6  =  " + ArrayFunHouse.getSum(one,3,6));
-		System.out.println("sum of spots 2-9  =  " + ArrayFunHouse.getSum(one,2,9));
-		System.out.println("# of 4s  =  " + ArrayFunHouse.getCount(one,4));
-		System.out.println("# of 9s  =  " + ArrayFunHouse.getCount(one,9));
+		System.out.println("sum of spots 3-6  =  " + test.getSum(3,6));
+		System.out.println("sum of spots 2-9  =  " + test.getSum(2,9));
+		System.out.println("# of 4s  =  " + test.getCount(4));
+		System.out.println("# of 9s  =  " + test.getCount(9));
 
 		int[] two = {4,2,3,4,6,7,8,9,0,10,0,1,7,6,5,3,2,9,9,8,7};
 
