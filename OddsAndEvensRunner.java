fed476edad03cb18fd69fea120c54f1bb3fe061d
MrsHines <34042849+MrsHines@users.noreply.github.com>
2019-01-08 13:04:55 -0600

    Starter File

--- OddsAndEvensRunner.java
+++ OddsAndEvensRunner.java
@@ -1,13 +1,14 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
 //Lab  -
 
-public class OddAnEvensRunner
+public class OddAndEvensRunner
 {
 	public static void main( String args[] )
 	{
 		//add test cases
+		//the methods in OddAndEvens are all static. Use the class name to call the methods.
 	}
 }
